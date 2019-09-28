57b774fb96051f7289e0bc78687b681ccba50250
Nho Huynh <nhohb07@gmail.com>
2019-09-28 16:29:51 +0700

    update test CI

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hello world',
+              'Hello World!',
             ),
           ],
         ),
